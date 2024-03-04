066dd6ce690ca4d56ae8ecb9973ce875263c901c
David <davidcheung231@gmail.com>
2024-03-04 16:30:15 -0600

    temperature flask done

--- app.py
+++ app.py
@@ -47,15 +47,71 @@ def Home():
 
 @app.route("/api/v1.0/precipitation")
 def Precipitation():
+    # year of the last entry
     last_entry = session.query(measurement.date).order_by(measurement.date.desc()).first()
+
+    # one year before the last entry
     year_before_last_entry = dt.datetime.strptime(last_entry[0], "%Y-%m-%d") - dt.timedelta(days = 366) 
+
+    # all the data within the last year
     precip_data_query = session.query(measurement.date, measurement.prcp).filter(measurement.date >= year_before_last_entry).all()
 
+    # dictionary data
     precip_data = {}
     for row in precip_data_query:
         precip_data[row[0]] = row[1]
 
     return jsonify(precip_data)
+
+@app.route("/api/v1.0/stations")
+def Stations():
+    station_data_query = session.query(station).all()
+    
+    station_data = []
+
+    # go through all stations
+    for data in station_data_query:
+
+        # get the data for one particular station
+        thisData = {
+            "station": data.station,
+            "name": data.name,
+            "latitude": data.latitude,
+            "longitutde": data.longitude,
+            "elevation": data.elevation
+        }
+
+        # store data into list
+        station_data.append(thisData)
+    
+    return jsonify(station_data)
+
+@app.route("/api/v1.0/tobs")
+def Tobs():
+    # get the most active
+    most_active_station = session.query(measurement.station).group_by(measurement.station).order_by(func.count(measurement.tobs).desc()).first()[0]
+
+    # year of the last entry
+    last_entry = session.query(measurement.date).order_by(measurement.date.desc()).first()
+
+    # one year before the last entry
+    year_before_last_entry = dt.datetime.strptime(last_entry[0], "%Y-%m-%d") - dt.timedelta(days = 366) 
+
+    # query all dates and temp of the most active station where date>= one year before the last entry.
+    dates_temp_query = session.query(measurement.date, measurement.tobs)\
+                        .filter(measurement.station == most_active_station)\
+                        .filter(measurement.date >= year_before_last_entry)
+    
+    dates_temp_data = []
+
+    for data in dates_temp_query:
+        thisData = {
+            "Date": data[0],
+            "Temperature": data[1]
+        }
+        dates_temp_data.append(thisData)
+
+    return jsonify(dates_temp_data)
 
 if __name__ == "__main__":
     app.run(debug = True)
